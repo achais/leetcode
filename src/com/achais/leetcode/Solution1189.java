0af1e436f8d1d623afbea05f489f1b42b10816de
achais <i@achais.com>
2020-08-21 11:27:12 +0800

    2020.08.21

---
+++ src/com/achais/leetcode/Solution1189.java
@@ -0,0 +1,37 @@
+package com.achais.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution1189 {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : text.toCharArray()) {
+            map.merge(c, 1, Integer::sum);
+        }
+
+        int[] arr = {
+                map.getOrDefault('b', 0),
+                map.getOrDefault('a', 0),
+                map.getOrDefault('l', 0) / 2,
+                map.getOrDefault('o', 0) / 2,
+                map.getOrDefault('n', 0),
+        };
+        int ans = Integer.MAX_VALUE;
+        for (int i : arr) {
+            ans = Math.min(ans, i);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String stdin = "loonbalxballpoon";
+
+        Solution1189 solution = new Solution1189();
+        Integer stdout = solution.maxNumberOfBalloons(stdin);
+
+        System.out.println(stdout);
+    }
+}
